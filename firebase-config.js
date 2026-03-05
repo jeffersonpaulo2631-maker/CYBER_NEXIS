d496302be4114894f6ee2953121520bad90a144d
jeffersonpaulo2631-maker <jeffersonpaulo2631@gmail.com>
2026-03-04 23:08:29 -0300

    Upgrade Firebase SDK and update config values
    Updated Firebase SDK version and modified configuration details.

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,15 @@
-// firebase-config.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  authDomain: "cybernexislogin.firebaseapp.com",
+  projectId: "cybernexislogin",
+  storageBucket: "cybernexislogin.firebasestorage.app",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-getAnalytics(app);
